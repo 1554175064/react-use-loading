37404690365b52a9a2ee5225501e6c09be117e91
jiayuan.gao <jiayuan.gao@adminer.cn>
2023-06-12 10:49:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+const react = require("react");
+
+const useLoading = (asyncFn) => {
+  const [isLoading, setIsLoading] = react.useState(false);
+  const fn = react.useCallback(
+    function () {
+      return new Promise((res, rej) => {
+        setIsLoading(true);
+        asyncFn(...arguments)
+          .then((data) => {
+            res(data);
+          })
+          .catch((err) => {
+            rej(err);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+      });
+    },
+    [asyncFn]
+  );
+  return [isLoading, fn];
+};
+exports.default = useLoading;
